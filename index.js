c07eb3cc3af3d767ca97d6cd0c54010a0f785d94
DylanParenzee <dylanparenzee@hotmail.com>
2022-05-21 12:22:13 +1000

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+ const add = (num1,num2) => {
+     return num1 + num2
+ }
+
+ const subtract =(num1, num2) => {
+     return num1 - num2; 
+ }
+
+ const multiply = (num1,num2) => {
+     return num1 * num2
+ }
+
+ const divide = (num1, num2) => {
+     return num1 / num2
+ }
+
+ const increment = (n) => {
+     return n +=1;
+ }
+
+ const decrement = (n) => {
+     return n -=1;
+ }
+
+const makeInt = (n) => {
+    return parseInt(n, 10)
+}
+
+const preserveDecimal = (n) => {
+    return parseFloat(n)
+}
